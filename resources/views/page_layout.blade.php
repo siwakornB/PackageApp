c8317b5e5ecf24d4b11fb31e3f1a6e39630bd524
ANGELOV0659 <tee2464za@hotmail.com>
2020-07-28 17:28:38 +0700

    new font

--- resources/views/page_layout.blade.php
+++ resources/views/page_layout.blade.php
@@ -33,6 +33,7 @@
     
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Prompt" rel="stylesheet">
     
     <!-- Custom CSS -->
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css')}}" >
